d80037f31a6218b750cf63e66ea2887c74dbd5a8
elipinska <ewa.lipinska@hotmail.com>
2018-06-19 11:47:11 +0100

    Make currency case insensitive

--- lib/exchange_rate/fx_calculator.rb
+++ lib/exchange_rate/fx_calculator.rb
@@ -6,17 +6,18 @@ module ExchangeRate
 
     def self.rate_at_date(date, base_curr, counter_curr)
       fx_data = DataCache.instance.read("fx_data") ? DataCache.instance.read("fx_data") : XMLParser.instance.fetch_and_save_fx_data
+
       date_string = date.to_s
 
       if !fx_data[date_string]
         raise NotFoundError.new(date_string)
       else
         [base_curr, counter_curr].each do |curr|
-          raise NotFoundError.new(curr) if !fx_data[date_string][curr]
+          raise NotFoundError.new(curr) if !fx_data[date_string][curr.upcase]
         end
       end
 
-      exchange_rate = BigDecimal(fx_data[date_string][counter_curr]) / BigDecimal(fx_data[date_string][base_curr])
+      exchange_rate = BigDecimal(fx_data[date_string][counter_curr.upcase]) / BigDecimal(fx_data[date_string][base_curr.upcase])
       return exchange_rate
     end
 
