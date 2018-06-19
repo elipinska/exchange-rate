1de47c8bd8218df40b0175d2655c296b69ea86eb
elipinska <ewa.lipinska@hotmail.com>
2018-06-19 11:33:56 +0100

    Remove unused cli method

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -10,13 +10,5 @@ module ExchangeRate
       puts ExchangeRate.fetch_rates
     end
 
-
-    # desc "at DATE BASE_CURR COUNTER_CURR", "Provides the FX rate between two currencies at specific date"
-    #
-    # method_option :date, :default => Date.today
-    # def at(date, base_curr, counter_curr)
-    #   puts ExchangeRate.at(date, base_curr, counter_curr)
-    # end
-
   end
 end
