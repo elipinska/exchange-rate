cb7cb35cf1ae222ec9ed97a56b238d6f770a7e2d
elipinska <ewa.lipinska@hotmail.com>
2018-06-21 00:28:28 +0100

    Update cli

--- lib/cli/cli.rb
+++ lib/cli/cli.rb
@@ -4,10 +4,15 @@ require 'exchange_rate'
 module ExchangeRate
   class CLI < Thor
 
-    desc "fetch_rates", "Retrieves data from XML source"
+    desc "fetch_rates", "Fetches data from XML store"
 
     def fetch_rates
       ExchangeRate.fetch_rates
+    end
+
+    desc "get_store", "Gets all stored data"
+
+    def get_store
       p DataCache.instance.store
     end
 
